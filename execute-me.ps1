2a0c404af52e6e3868b4267225bea30a9e7f0d18
Jon Marco Reyes <rjonmarco@gmail.com>
2020-06-18 22:54:35 +0800

    Powershell Initial Push
    PowerShell is not yet working as expected.
    - Does not launch Steam with parameters attached
    
    -Removed Microsoft Batch Documentation References.

---
+++ execute-me.ps1
@@ -0,0 +1,28 @@
+﻿#to run this you must agree to set execution policy to Unrestricted
+#For more info on this command: https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_execution_policies?view=powershell-7
+#Run Powershell as Administration and execute "Set-ExecutionPolicy Unrestricted"
+
+#set this variable to your steam location
+$steamLocation = "C:\Program Files (x86)\Steam"
+
+#appID 730 is CSGO
+#Find AppID here https://steamdb.info/apps/
+$appID = "730"
+
+#Kills Steam Processes
+Stop-Process -force -processname steam
+Stop-Process -force -processname SteamService
+Stop-Process -force -processname steamwebhelper 
+
+#Change Directory to Steam folder
+Set-Location -Path $steamLocation
+
+#Delete DLLs to be redownloaded (force DL)
+Remove-Item Steam.dll
+Remove-Item SteamUI.dll
+
+# https://developer.valvesoftware.com/wiki/Command_Line_Options#Steam_.28Windows.29
+# -cafeapplaunch == Launch apps in a cyber cafe context (Forces apps to be verified / validated before launch). 
+# -applaunch Launches an Game or Application through Steam. 
+$args = "-cafeapplaunch -applaunch $appID"
+& $steamLocation\steam.exe $args
