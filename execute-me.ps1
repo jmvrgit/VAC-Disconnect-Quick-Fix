a9fc914c866b1ad7392b4dda642a80b24274f737
Jon Marco Reyes <rjonmarco@gmail.com>
2020-06-20 10:45:57 +0800

    Powershell Script now work
    A bit hackish implementation for the powershell. I'd still recommend the Batch File

--- execute-me.ps1
+++ execute-me.ps1
@@ -2,6 +2,18 @@
 #For more info on this command: https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_execution_policies?view=powershell-7
 #Run Powershell as Administration and execute "Set-ExecutionPolicy Unrestricted"
 
+#source:
+#https://blog.expta.com/2017/03/how-to-self-elevate-powershell-script.html
+# Self-elevate the script if required
+if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
+    if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
+        $CommandLine = "-File `"" + $MyInvocation.MyCommand.Path + "`" " + $MyInvocation.UnboundArguments
+        Start-Process -FilePath PowerShell.exe -Verb Runas -ArgumentList $CommandLine
+        Exit
+    }
+}
+
+
 #set this variable to your steam location
 $steamLocation = "C:\Program Files (x86)\Steam"
 
@@ -10,19 +22,18 @@ $steamLocation = "C:\Program Files (x86)\Steam"
 $appID = "730"
 
 #Kills Steam Processes
-Stop-Process -force -processname steam
 Stop-Process -force -processname SteamService
+Stop-Process -force -processname steam
 Stop-Process -force -processname steamwebhelper 
 
 #Change Directory to Steam folder
 Set-Location -Path $steamLocation
 
 #Delete DLLs to be redownloaded (force DL)
-Remove-Item Steam.dll
-Remove-Item SteamUI.dll
+Remove-Item Steam.dll -Force
+Remove-Item SteamUI.dll -Force
 
 # https://developer.valvesoftware.com/wiki/Command_Line_Options#Steam_.28Windows.29
 # -cafeapplaunch == Launch apps in a cyber cafe context (Forces apps to be verified / validated before launch). 
 # -applaunch Launches an Game or Application through Steam. 
-$args = "-cafeapplaunch -applaunch $appID"
-& $steamLocation\steam.exe $args
+cmd.exe /c "`"$steamLocation\Steam.exe`" -cafeapplaunch -applaunch $appID"
